d03fd29bf2d0ab3b1bd73ca22918fa032d59b0b1
adarsh-shahi <adarshshahi1009@gmail.com>
2023-04-06 16:57:34 +0530

    export flatObject feature

--- index.js
+++ index.js
@@ -28,82 +28,10 @@ function flattenObject(flattenedObject, complexObject, parentObjectName = "") {
 	}
 }
 
-// Define a complex object to be flattened
-const complexObject = {
-	id: 1,
-	name: "John Doe",
-	contact: {
-		email: "johndoe@example.com",
-		phone: {
-			home: "123-456-7890",
-			work: "098-765-4321",
-			getHomeAreaCode: function () {
-				return this.home.slice(0, 3);
-			},
-			getWorkAreaCode: function () {
-				return this.work.slice(0, 3);
-			},
-		},
-	},
-	address: {
-		street: "123 Main St",
-		city: "Anytown",
-		state: "CA",
-		zip: "12345",
-		country: "USA",
-		coordinates: {
-			latitude: 37.7749,
-			longitude: -122.4194,
-		},
-	},
-	orders: [
-		{
-			id: 100,
-			date: "2022-01-01",
-			items: [
-				{
-					id: 1,
-					name: "Product A",
-					price: 10.99,
-				},
-				{
-					id: 2,
-					name: "Product B",
-					price: 19.99,
-				},
-			],
-			getTotal: function () {
-				let total = 0;
-				this.items.forEach((item) => {
-					total += item.price;
-				});
-				return total;
-			},
-		},
-		{
-			id: 101,
-			date: "2022-01-05",
-			items: [
-				{
-					id: 3,
-					name: "Product C",
-					price: 14.99,
-				},
-			],
-			getTotal: function () {
-				let total = 0;
-				this.items.forEach((item) => {
-					total += item.price;
-				});
-				return total;
-			},
-		},
-	],
-};
-
-// Define an empty object to store the flattened object
-const flattenedObject = {};
-
-// Call the flattenObject function with the complex object and the empty object as arguments
-flattenObject(flattenedObject, complexObject);
-console.log(flattenedObject);
+export default function flatObject(complexObject) {
+	// Define an empty object to store the flattened object
+	const flattenedObject = {};
+	// Call the flattenObject function with the complex object and the empty object as arguments
+	flattenObject(flattenedObject, complexObject);
+	return flattenedObject;
+}
